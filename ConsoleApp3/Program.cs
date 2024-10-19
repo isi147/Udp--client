df27c68bebfa46944cf70a27f99a0d218f39c472
Seyidmammadli_i <Seyidmammadli_i@STHQ0121-01>
2024-10-19 11:12:39 +0400

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+var client = new Socket(AddressFamily.InterNetwork,
+                        SocketType.Dgram,
+                        ProtocolType.Udp);
+
+var ip = IPAddress.Parse("127.0.0.1");
+var port = 45678;
+var remoteEP = new IPEndPoint(ip, port);
+
+var msg = string.Empty;
+var buffer = Array.Empty<byte>();
+
+while (true)
+{
+    msg = Console.ReadLine() ?? string.Empty;
+    buffer = Encoding.Default.GetBytes(msg);
+    client.SendTo(buffer, remoteEP);
+}
